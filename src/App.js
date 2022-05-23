86ccca3bd008d7d4ae4477f9008331ac0ed6d03b
Mostafa Samir <mostafasamer1999@hotmail.com>
2022-05-24 01:24:44 +0200

    Create and Test btn-primary Component

--- src/App.js
+++ src/App.js
@@ -2,10 +2,15 @@ import React from 'react';
 import './App.scss';
 import '../src/assets/styles/global.scss'
 
+import BtnPrimary from "./app/components/btn-primary/btn-primary"
+
 function App() {
   return (
     <div className="App">
       Mostafa Samir
+      <div style={{width: "33%"}}>
+        <BtnPrimary text="Click Me!" onClick={() => { alert("OK") }} />
+      </div>
     </div>
   );
 }
---
+++ src/app/components/btn-primary/btn-primary.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+function BtnPrimary(props) {
+
+    const { text, onClick } = props;
+
+    const StyledButton = styled.button`
+        background-color: var(--wed-black);
+        color: var(--wed-white);
+        cursor: pointer;
+
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem;
+        padding-left: 2rem;
+        padding-right: 2rem;
+
+        border-radius: 0.5rem;
+        width: 80%;
+        box-shadow: 0 4px 6px -1px rgba(0,0,0,0.1),0 2px 4px -1px rgba(0,0,0,0.06);
+
+        &:hover {
+            font-weight: var(--font-bold);
+            box-shadow: 0 20px 25px -5px rgba(0,0,0,0.1),0 10px 10px -5px rgba(0,0,0,0.04);
+        }
+    `;
+
+    return (
+        <StyledButton onClick={onClick} className="text-sm">
+            {text}
+        </StyledButton>
+    );
+}
+
+export default BtnPrimary;

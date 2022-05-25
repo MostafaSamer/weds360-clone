0de2f2a8e4fc3f2f9c54612e4ca971c161b7f04b
Mostafa Samir <mostafasamer1999@hotmail.com>
2022-05-25 13:41:40 +0200

    Finish PhototCard Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.scss';
 import '../src/assets/styles/global.scss'
 
 import Navbar from "./app/components/navbar/navbar"
+import PhotoCard from "./app/components/photocard/photocard"
 // import BtnPrimary from "./app/components/btn-primary/btn-primary"
 import Footer from "./app/components/footer/footer"
 
@@ -12,6 +13,7 @@ function App() {
       <div>
         <Navbar />
         {/* <BtnPrimary text="Click Me!" onClick={() => { alert("OK") }} /> */}
+        <PhotoCard />
         <br />
         <br />
         <br />
---
+++ src/app/components/photocard/photocard.js
@@ -0,0 +1,12 @@
+import styles from "./photocard.module.scss"
+
+function PhotoCard() {
+    return (
+        <div className={styles.PhotoContainer + " h-full rounded-lg overflow-hidden shadow-md hover:shadow-lg"} style={{width: "290px"}}>
+            <img className="lg:h-36 md:h-36 w-full object-cover object-center" src="https://weds360-production.s3.amazonaws.com/store/photo/431/src/large-6792d00acebc42fc36a6abba09f4513b.jpeg" alt="Irresistible appetizers’ corner" />
+            <div className="p-2"><h1 className={styles.PhotoCardHeader + " text-center  text-lg font-semibold"}>Irresistible appetizers’ corner</h1></div>
+        </div>
+    )
+}
+
+export default PhotoCard;

b739a77a477463c26712d02887072651e7bee778
Mostafa Samir <mostafasamer1999@hotmail.com>
2022-05-25 14:39:36 +0200

    Finish TreeView Component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import '../src/assets/styles/global.scss'
 import Navbar from "./app/components/navbar/navbar"
 import PhotoCard from "./app/components/photocard/photocard"
 import PhotoGrid from "./app/components/photogrid/photogrid"
+import TreeView from "./app/components/treeview/treeview"
 // import BtnPrimary from "./app/components/btn-primary/btn-primary"
 import Footer from "./app/components/footer/footer"
 
@@ -14,23 +15,24 @@ function App() {
       <div>
         <Navbar />
         {/* <BtnPrimary text="Click Me!" onClick={() => { alert("OK") }} /> */}
-        <PhotoGrid>
+        {/* <PhotoGrid>
           <PhotoCard />
           <PhotoCard />
           <PhotoCard />
           <PhotoCard />
           <PhotoCard />
           <PhotoCard />
-        </PhotoGrid>
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
-        <br />
+        </PhotoGrid> */}
+        <div>
+          <div>
+            <div><TreeView element={[
+              {name: "Galary", state: "https://weds360.com/en/moreWeddings/"},
+              {name: "Wedding Ideas", state: "#"}
+            ]} /></div>
+            <div></div>
+            <div></div>
+          </div>
+        </div>
         <Footer />
       </div>
     </div>
---
+++ src/app/components/treeview/treeview.js
@@ -0,0 +1,29 @@
+import styles from "./treeview.module.scss"
+
+function TreeView({ element }) {
+    return (
+        <div className="lg:w-9/12 lg:mx-auto pt-5 px-10 lg:pl-0">
+            <nav className="MuiTypography-root MuiTypography-body1 MuiBreadcrumbs-root css-o3d33y" aria-label="breadcrumb">
+                <ol className="MuiBreadcrumbs-ol css-nhb8h9">
+                    {element.map((el, index) => {
+                        return (
+                            <>
+                                {index == 0 && <li className="MuiBreadcrumbs-li">
+                                    <a href={el.state} className={styles.TreeElementAnchor + " font-bold text-lg"}>{el.name}</a>
+                                </li>}
+                                {index !== 0 && <>
+                                    <li aria-hidden="true" className="MuiBreadcrumbs-separator css-3mf706">›</li>
+                                    <li className="MuiBreadcrumbs-li">
+                                        <p className={styles.TreeElementPara + " MuiTypography-root MuiTypography-body1 css-9l3uo3"}>{el.name}</p>
+                                    </li>
+                                </>}
+                            </>
+                        )
+                    })}
+                </ol>
+            </nav>
+        </div>
+    )
+}
+
+export default TreeView;
